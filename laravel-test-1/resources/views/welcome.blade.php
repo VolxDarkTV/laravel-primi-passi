f67190c2ba64225fe55b266940c02001000df18d
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-27 14:33:24 +0100

    first commit

---
+++ laravel-test-1/resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="../css/app.css" href="styles.css" />
+    <title>Laravel</title>
+</head>
+<body>
+    <div>
+        <div class="container">
+            <span>Hello World</span>
+        </div>
+    </div>
+</body>
+</html>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .container{
+        color: aqua;
+        display:flex;
+        justify-content:center;
+    }
+
+</style>
